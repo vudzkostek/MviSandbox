8658e0658934530a35330348eb28bd16b5338134
Artur Kosta <artur.kosta@droidsonroids.pl>
2020-06-18 11:05:44 +0200

    Revert "More products"
    This reverts commit 27de7cb9735a2998411491e6c0a7a12c6a91db4f.

--- app/src/main/java/com/arturkosta/mvisandbox/repository/ProductsRepository.kt
+++ app/src/main/java/com/arturkosta/mvisandbox/repository/ProductsRepository.kt
@@ -19,13 +19,7 @@ class ProductsRepository @Inject constructor() : ProductListRepository, ProductD
         product(7),
         product(8),
         product(9),
-        product(10),
-        product(101),
-        product(11),
-        product(112),
-        product(12),
-        product(123),
-        product(13)
+        product(10)
     )
 
     private val productsDetails = mutableListOf(
@@ -62,6 +56,5 @@ class ProductsRepository @Inject constructor() : ProductListRepository, ProductD
     }
 
     private fun product(id: Int) = Product(id = id, title = "Title $id", price = "$id,$id$")
-    private fun productDetails(id: Int) =
-        ProductDetails(id = id, title = "Title $id", description = "Description $id")
+    private fun productDetails(id: Int) = ProductDetails(id = id, title = "Title $id", description = "Description $id")
 }
